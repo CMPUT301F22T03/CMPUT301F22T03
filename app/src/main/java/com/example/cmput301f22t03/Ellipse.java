69f7fee3ddbb300d9c653e7a0cfdf66332ce3992
Fan Zhu <fzhu@ualberta.ca>
2022-10-01 16:01:53 -0600

    git c

--- app/src/main/java/com/example/cmput301f22t03/Ellipse.java
+++ app/src/main/java/com/example/cmput301f22t03/Ellipse.java
@@ -4,8 +4,9 @@ public class Ellipse extends Shape{
     private Integer a;
     private Integer b;
 
-    public Rectangle(Integer x, Integer y, Integer a, Integer b) {
-        super(x, y);
+    public Ellipse(Integer x, Integer y, Integer a, Integer b) {
+        this.x = x;
+        this.y = y;
         this.a = a;
         this.b = b;
     }
