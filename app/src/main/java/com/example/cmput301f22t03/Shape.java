59406f17fa2a838e17f84ef62a622a35fceaa591
Yuxuan Yang <97766395+YuxuanYang2002@users.noreply.github.com>
2022-10-01 17:58:15 -0600

    Update Shape.java

--- app/src/main/java/com/example/cmput301f22t03/Shape.java
+++ app/src/main/java/com/example/cmput301f22t03/Shape.java
@@ -3,6 +3,6 @@ package com.example.cmput301f22t03;
 public abstract class Shape {
     public Integer x;
     public Integer y;
-    public String color = "yellow";
+    public String color = "black";
 
 }
