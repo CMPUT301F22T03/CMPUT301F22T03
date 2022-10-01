95e1a3a0db12be6fccbcde3edb7ca911cf023943
“Mashiad <mashiad@ualberta.ca>
2022-10-01 13:55:20 -0600

    Added Square class.

---
+++ app/src/main/java/com/example/cmput301f22t03/Rectangle.java
@@ -0,0 +1,12 @@
+package com.example.cmput301f22t03;
+
+public class Square extends Shape {
+    private int side;
+
+    public Square(Integer x, Integer y, Integer side) {
+        this.x = x;
+        this.y = y;
+        this.side = side;
+    }
+}    
+
