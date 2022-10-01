d64d2925f1c478cee60baf0818ad07138b2d5ca3
Xiangxu Meng <xiangxu@ualberta.ca>
2022-10-01 15:44:04 -0600

    create a star object

---
+++ app/src/main/java/com/example/cmput301f22t03/Star.java
@@ -0,0 +1,11 @@
+package com.example.cmput301f22t03;
+
+public class Star extends Shape{
+    private int angle;
+
+    public Star(Integer x,Integer y,Integer angle) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+    }
+}
